2cba18bb8619f4a01a1c3a60616fca818de302cc
Itsede Okhai <itsedeokhai7@gmail.com>
2023-02-17 01:50:56 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  Entry point
+ * Description: Get a random it with 5
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'a';
+	while(c >= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putcar('\n');
+	return (0);
+}
