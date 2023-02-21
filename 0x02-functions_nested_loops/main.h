a71ccc008da031989ffc80ee9e5d4d28892be013
Itsede Okhai <itsedeokhai7@gmail.com>
2023-02-21 03:57:54 -0800

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifindef main_h
+#ifndef main_h
 #define main_h
 int _putchar(char);
 void print_alphabet(void);
@@ -8,8 +8,9 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_bauer(void);
+void jack_baueir(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 #endif
