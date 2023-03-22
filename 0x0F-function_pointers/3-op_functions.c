0dccceadb3c08e2e012ad0f5f9ba0badf4eed54b
Itsede Okhai <itsedeokhai7@gmail.com>
2023-03-22 01:59:47 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,32 +1,67 @@
-#include <stdlib.h>
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * get_op_func - selects the correct function to perform
- * the operation asked by the user
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
  *
- * @s: operator string
- * Return: int
+ * Return: integer
  */
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+	return (a + b);
+}
 
-	while (i < 5)
-	{
-		if (*s == *(ops[i]).op && *(s + 1) == '\0')
-		{
-			return (ops[i].f);
-		}
-		i++;
-	}
-	return (NULL);
+/**
+ * op_sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the modulus of two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+
 }
