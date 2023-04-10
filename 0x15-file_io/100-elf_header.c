d7288ef7523b5acbfe7c2ee3c9d50db43c051e2e
Itsede Okhai <itsedeokhai7@gmail.com>
2023-04-10 15:38:26 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -18,10 +18,10 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
- * check_elf - Checks if a file is an ELF file
- * @e_ident: A pointer to an array containing the ELF magic numbers
+ * check_elf - Checks if a file is an ELF file.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
- * Itsede Okhai.
+ * Itsede Okhai project.
  * Description: If the file is not an ELF file - exit code 98
  */
 void check_elf(unsigned char *e_ident)
@@ -42,8 +42,8 @@ exit(98);
 }
 
 /**
- * print_magic - Prints the magic numbers of an ELF header
- * @e_ident: A pointer to an array containing the ELF magic numbers
+ * print_magic - Prints the magic numbers of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
  * Itsede project.
  * Description: Magic numbers are separated by spaces
@@ -71,22 +71,21 @@ printf(" ");
  */
 void print_class(unsigned char *e_ident)
 {
-	printf("  Class:                             ");
-
-	switch (e_ident[EI_CLASS])
-	{
-	case ELFCLASSNONE:
-		printf("none\n");
-		break;
-	case ELFCLASS32:
-		printf("ELF32\n");
-		break;
-	case ELFCLASS64:
-		printf("ELF64\n");
-		break;
-	default:
-		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
-	}
+printf("Class:");
+switch (e_ident[EI_CLASS])
+{
+case ELFCLASSNONE:
+printf("none\n");
+break;
+case ELFCLASS32:
+printf("ELF32\n");
+break;
+case ELFCLASS64:
+printf("ELF64\n");
+break;
+default:
+printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+}
 }
 
 /**
@@ -95,7 +94,7 @@ void print_class(unsigned char *e_ident)
  */
 void print_data(unsigned char *e_ident)
 {
-	printf("  Data:                              ");
+	printf("Data:");
 
 	switch (e_ident[EI_DATA])
 	{
@@ -119,7 +118,7 @@ void print_data(unsigned char *e_ident)
  */
 void print_version(unsigned char *e_ident)
 {
-	printf("  Version:                           %d",
+	printf("Version:%d",
 	       e_ident[EI_VERSION]);
 
 	switch (e_ident[EI_VERSION])
@@ -139,7 +138,7 @@ void print_version(unsigned char *e_ident)
  */
 void print_osabi(unsigned char *e_ident)
 {
-	printf("  OS/ABI:                            ");
+	printf("OS/ABI:");
 
 	switch (e_ident[EI_OSABI])
 	{
@@ -184,7 +183,7 @@ void print_osabi(unsigned char *e_ident)
  */
 void print_abi(unsigned char *e_ident)
 {
-	printf("  ABI Version:                       %d\n",
+	printf("ABI Version:%d\n",
 	       e_ident[EI_ABIVERSION]);
 }
 
@@ -198,7 +197,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
 
-	printf("  Type:                              ");
+	printf("Type:");
 
 	switch (e_type)
 	{
@@ -229,7 +228,7 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
  */
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 {
-	printf("  Entry point address:               ");
+	printf("Entry point address:");
 
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
