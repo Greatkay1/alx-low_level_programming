5dc1a6ddaef8a21b03ef463704806642e5d05ea5
Itsede Okhai <itsedeokhai7@gmail.com>
2023-04-10 14:34:46 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,32 +1,14 @@
 #include "main.h"
 /**
- * _strlen - finds the length of a string
- * @str: pointer to the string.
- * Itsede Okhai project.
- *
- * Return: length of the string.
- */
-size_t _strlen(char *str)
-{
-size_t i;
-
-for (i = 0; str[i]; i++);
-
-return (i);
-}
-
-/**
- * append_text_to_file - appends a text at the end of a file.
- * @filename: name of the file
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: name of the file.
  * @text_content: NULL terminated string to add at the end of the file.
- * Itsede project.
- *
- * Return: 1 on success and -1 on failure
+ * Itsede Okhai project.
+ * Return: 1 on the success, -1 on failure.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd;
-ssize_t len;
+int fd, bytes_written = 0;
 
 if (filename == NULL)
 return (-1);
@@ -34,9 +16,12 @@ fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 if (text_content != NULL)
-len = write(fd, text_content, _strlen(text_content));
-close(fd);
-if (len == -1)
+{
+while (text_content[bytes_written] != '\0')
+bytes_written++;
+if (write(fd, text_content, bytes_written) != bytes_written)
 return (-1);
+}
+close(fd);
 return (1);
 }
